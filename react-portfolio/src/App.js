a72968a6ee05ae422a2816bacd82990bd9782885
hawkiiie729 <mishra.firehawk729@gmail.com>
2022-05-03 14:42:24 +0530

    Created Navabr component

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar";
+
+
+
+function App() {
+  return (
+    <div >
+    <h1 className="text-2xl font-bold">
+      <Navbar/>
+    </h1>
+    </div>
+  );
+}
+
+export default App;
